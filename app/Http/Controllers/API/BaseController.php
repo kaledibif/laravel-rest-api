daff88a00b92be5750c2531bde8afd8e89a50a97
Faruk K <kaledibif@gmail.com>
2019-10-26 19:55:33 +0000

    Laravel Passport library added

---
+++ app/Http/Controllers/API/BaseController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller as Controller;
+use Illuminate\Http\Response;
+
+class BaseController extends Controller
+{
+    /**
+     * Success response method.
+     *
+     * @param $result
+     * @param $message
+     * @return Response
+     */
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message,
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Error response method.
+     *
+     * @param $error
+     * @param array $errorMessages
+     * @param int $code
+     * @return Response
+     */
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if (!empty($errorMessages)) {
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+}
