8e91c621cd0cad5f1db2049bb895874b841da121
kaledibi <kaledibif@gmail.com>
2019-11-05 22:12:29 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,24 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::post('register', 'API\RegisterController@register');
+Route::post('login', 'API\AuthController@login');
+Route::post('register', 'API\AuthController@register');
 
+// Password Reset
+Route::post('forgot-password', 'API\AuthController@forgotPassword');
+Route::get('reset-password-confirm/{token}', 'API\AuthController@resetPasswordConfirm');
+Route::post('reset-password', 'API\AuthController@resetPassword');
+
+// API Middleware
 Route::middleware('auth:api')->group(function () {
+    // Auth
+    Route::get('profile', 'API\AuthController@profile');
+    Route::get('logout', 'API\AuthController@logout');
+
+    // Resources
     Route::resource('categories', 'API\CategoryController');
     Route::resource('items', 'API\ItemController');
     Route::resource('colors', 'API\ColorController');
     Route::resource('icons', 'API\IconController');
+    Route::resource('images', 'API\ImageController');
 });
