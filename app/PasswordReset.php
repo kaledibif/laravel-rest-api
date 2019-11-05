5efed623402135a7bdebb04837e69c8a4895d556
kaledibi <kaledibif@gmail.com>
2019-11-05 22:12:33 +0300

    Create PasswordReset.php

---
+++ app/PasswordReset.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PasswordReset extends Model
+{
+    const UPDATED_AT = null;
+
+    protected $fillable = [
+        'email',
+        'token',
+    ];
+}
